c35eb3161029c5abfd611a57b46947ddee50870f
Apurv Gupta <186622906+Apurv7Gupta@users.noreply.github.com>
2026-03-08 20:28:44 +0530

    Create Numbers_cont.cpp

---
+++ C++/Patterns/Numbers_cont.cpp
@@ -0,0 +1,19 @@
+int n = 25;
+    
+int row = 1;
+    
+int count = 1;
+    
+    
+    while(row<=5){
+    
+    int col = 1;        
+        while(col<=n){
+            cout<<count<<",";
+            count++;
+            col++;
+        }
+        
+        cout<<endl;
+        row++;
+    }
