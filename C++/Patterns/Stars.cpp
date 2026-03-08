28eb90a721fe34b9902bac7b11088f38e60c446a
Apurv Gupta <186622906+Apurv7Gupta@users.noreply.github.com>
2026-03-08 19:35:17 +0530

    Create Stars.cpp

---
+++ C++/Patterns/Stars.cpp
@@ -0,0 +1,22 @@
+// print 5 stars in a line
+//then endl
+
+//put it all in a loop that runs 5 times
+
+int n = 5;
+
+int row = 1;
+
+while(row<=n){
+    
+    int col = 1;
+    
+    while(col<=n){
+        cout<<"* ";
+        
+        col++;
+    }
+    
+    cout<<endl;
+    row++;
+}
