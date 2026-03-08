c33cc77842387cf4a2493721965d4f1ad486267a
Apurv Gupta <186622906+Apurv7Gupta@users.noreply.github.com>
2026-03-08 20:15:35 +0530

    Create Numbers.cpp

---
+++ C++/Patterns/Numbers.cpp
@@ -0,0 +1,21 @@
+// Use same method as Stars.cpp, but print the column itself
+
+int n = 5;
+    
+int row = 1;
+    
+    while(row<=n){
+        
+        
+        int col = 1;
+        while(col<=n){
+        
+        cout<<col;
+            
+        col++;
+        }
+        
+        cout<<endl;
+        
+        row++;
+    }
