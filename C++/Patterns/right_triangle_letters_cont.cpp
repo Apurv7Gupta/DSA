193e7764fdfe34a8a89b6e106a9978418ebf13fb
Apurv Gupta <186622906+Apurv7Gupta@users.noreply.github.com>
2026-03-19 14:35:51 +0530

    Create right_triangle_letters_cont.cpp

---
+++ C++/Patterns/right_triangle_letters_cont.cpp
@@ -0,0 +1,25 @@
+/*
+
+A
+BC
+DEF
+GHIJ
+
+*/
+
+ int n = 4;
+    int row = 1;
+    char letter = 'A';
+
+    while (row <= n)
+    {
+        int col = 1;
+        while (col <= row)
+        {
+            cout << letter;
+            letter++;
+            col++;
+        }
+        cout << endl;
+        row++;
+    }
