16078c34b70cf9077d68c194a89834c4b6e0511f
Apurv7Gupta <186622906+Apurv7Gupta@users.noreply.github.com>
2026-03-28 02:18:23 +0530

    ADD: Binary search visualizer file

--- C++/LeetCode/Techniques/Searching/LinearSearch.cpp
+++ C++/LeetCode/Techniques/Searching/LinearSearch.cpp
@@ -1,10 +1,11 @@
-int linearSearch(const std::vector<int>& arr, int target) {
-    for (int i = 0; i < static_cast<int>(arr.size()); ++i) {
+int linearSearch(const vector<int> &arr, int target)
+{
+    for (int i = 0; i < static_cast<int>(arr.size()); ++i)
+    {
         if (arr[i] == target)
             return i; // return index of found element
     }
     return -1; // target not found
 }
-
 
 // O(n) time, O(1) space
