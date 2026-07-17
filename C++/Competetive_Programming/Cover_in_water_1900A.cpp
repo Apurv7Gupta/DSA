e6645bd6eb2033e2ccaa5c42f895762dc6a3a458
Apurv7Gupta <186622906+Apurv7Gupta@users.noreply.github.com>
2026-07-18 00:57:19 +0530

    Add: CP 1900A

--- C++/Competetive_Programming/Cover_in_water_1900A.cpp
+++ C++/Competetive_Programming/Cover_in_water_1900A.cpp
@@ -1,3 +1,18 @@
+/*
+
+if you got three consecutive dots "..."
+
+then:
+
+1) fill first and last indices of the three consec dots using Action 1 "W.W" -> middle dot fills automatically -> "WWW"
+
+2) now you can just take the middle dot's water and fill any other cell using Action 2
+
+This is why no matter what the string is, if you have "..." the answer will always be 2, because Action 1 will only be performed 2 times
+And Action 2 is not counted
+
+*/
+
 void solve()
 {
     // ================inputs==============
@@ -10,7 +25,7 @@ void solve()
     int count = 0;
 
     if (s.find("...") != string::npos)
-        cout << 2;
+        cout << 2 << "\n";
 
     else
     {
@@ -19,6 +34,6 @@ void solve()
             if (s[i] == '.')
                 count++;
         }
-        cout << count;
+        cout << count << "\n";
     }
 }
